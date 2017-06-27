b4bb0b636dabe612071fadaef0dd891a8cbb6821
zhizunbao <375344563@qq.com>
2017-06-27 19:03:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,21 +13,11 @@ import os
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 execute(["scrapy","crawl","jobbole"])
-# execute(["scrapy", "crawl", "zhihu"])
+#execute(["scrapy", "crawl", "zhihu"])
 #execute(["scrapy", "crawl", "lagou"])
 
 
 
 
-
-
-# def remove_comment_tags():
-#     z = {'a': 1, 'b': 2, 'c': 3}
-#     lst=["aaa","dfdd"]
-#     lst.remove("aaja")
-#     print( lst)
-#
-#
-#
 # if __name__=="__main__":
 #     remove_comment_tags()
